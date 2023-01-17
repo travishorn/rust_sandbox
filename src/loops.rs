02f312c4f4c84920774a8ae2dfabb52b61e9de1c
Travis Horn <travis@travishorn.com>
2023-01-17 13:40:44 -0600

    Added loops

---
+++ src/loops.rs
@@ -0,0 +1,53 @@
+// Iterate until a condition is met
+
+pub fn run() {
+    let mut count = 0;
+
+    // Conditional break
+    loop {
+        count += 1;
+        println!("Count: {}", count);
+
+        if count == 20 {
+            break;
+        }
+    }
+
+    println!("----------");
+    println!("While loop");
+    println!("----------");
+
+    count = 1;
+
+    // While
+    while count <= 100 {
+        if count % 15 == 0 {
+            println!("FizzBuzz");
+        } else if count % 3 == 0 {
+            println!("Fizz");
+        } else if count % 5 == 0 {
+            println!("Buzz");
+        } else {
+            println!("{}", count);
+        }
+
+        count += 1;
+    }
+
+    println!("---------");
+    println!("For range");
+    println!("---------");
+
+    // For range
+    for x in 1..100 {
+        if x % 15 == 0 {
+            println!("FizzBuzz");
+        } else if x % 3 == 0 {
+            println!("Fizz");
+        } else if x % 5 == 0 {
+            println!("Buzz");
+        } else {
+            println!("{}", x);
+        }
+    }
+}
