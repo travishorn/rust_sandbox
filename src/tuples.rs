0fc6268d3d315e608b901c15848e3d51d71306fe
Travis Horn <travis@travishorn.com>
2023-01-17 10:05:59 -0600

    Added tuples

---
+++ src/tuples.rs
@@ -0,0 +1,9 @@
+// Grouped values of different types. Max 12 elements
+
+pub fn run() {
+    let person: (&str, &str, i8) = ("Mike", "New Jersey", 31);
+    println!(
+        "{} is from {} and is {} years old.",
+        person.0, person.1, person.2
+    );
+}
