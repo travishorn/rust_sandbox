8ba05ac7be8f3ab7f11041f9013b76c42ab98760
Travis Horn <travis@travishorn.com>
2023-01-17 14:53:18 -0600

    Added enums

---
+++ src/enums.rs
@@ -0,0 +1,27 @@
+// Types that have a few definite values
+
+enum Movement {
+    // Variants
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+fn move_avatar(m: Movement) {
+    // Perform action depending on variant
+    // match is like switch in other languages
+    match m {
+        Movement::Up => println!("Avatar moving up."),
+        Movement::Right => println!("Avatar moving right."),
+        Movement::Down => println!("Avatar moving down."),
+        Movement::Left => println!("Avatar moving left."),
+    }
+}
+
+pub fn run() {
+    move_avatar(Movement::Up);
+    move_avatar(Movement::Right);
+    move_avatar(Movement::Down);
+    move_avatar(Movement::Left);
+}
