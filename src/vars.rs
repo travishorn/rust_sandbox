cbb1c0310a3c2e7c8038c04434493210737fd8a1
Travis Horn <travis@travishorn.com>
2023-01-17 09:32:05 -0600

    Added vars

---
+++ src/vars.rs
@@ -0,0 +1,21 @@
+// Variables hold primitive data or references
+// Immutable and block-scoped
+
+pub fn run() {
+    let name = "Mike";
+    let mut age = 30;
+
+    println!("My name is {} and I am {} years old.", name, age);
+
+    age = 31;
+
+    println!("My name is {} and I am {} years old.", name, age);
+
+    // Constant - type required. Usually all caps.
+    const ID: i32 = 1;
+    println!("ID: {}", ID);
+
+    // Assign multiple vars
+    let (my_name, my_age) = ("Mike", 31);
+    println!("{} is {} years old.", my_name, my_age);
+}
